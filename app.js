ccedf6b2a7f28405dad17078cb0921aadf9047e4
Vinícius Alves <vinicius.alves0503@gmail.com>
2023-03-04 21:49:50 -0300

    Continuando o desenvolvimento do JS

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-function newTitle() {
+const close = document.getElementsByClassName("close");
+const done = document.getElementsByClassName("done");
+
+function newTask() {
     let li = document.createElement("li");
-    const inputValue = document.getElementById("inputTitle").value;
+    const inputValue = document.querySelector("#inputTask").value;
 
     let newLi = document.createTextNode(inputValue);
     li.appendChild(newLi);
 
-    inputValue === '' ? alert("You must write something!") : document.getElementById("titles").appendChild(li);
-    document.getElementById("inputTitle").value = "";
+    inputValue === '' ? alert("You didn't type anything") : document.querySelector("#tasks").appendChild(li);
+    document.querySelector("#inputTask").value = "";
 
-    let span = document.createElement("span");
-    span.className = "close";
-    let text = document.createTextNode("\u00D7");
+    for (let i = 0; i < 2; i++) {
 
-    span.appendChild(text);
-    li.appendChild(span);
+        if (i === 1) {
+            let span = document.createElement("span");
+            span.className = "close";
+            let text = document.createTextNode("\u274C");
+
+            span.appendChild(text);
+            li.appendChild(span);
+        }
+        else {
+            let span = document.createElement("span");
+            span.className = "done";
+            let text = document.createTextNode("\u2714\uFE0F");
+
+            span.appendChild(text);
+            li.appendChild(span);
+        }
+    }
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          let div = this.parentElement;
+          div.style.display = "none";
+        }
+    }
+
+    for (let i = 0; i < done.length; i++) {
+        done[i].onclick = function() {
+          let div = this.parentElement;
+          div.style.backgroundColor = "#FFFA00";
+        }
+    }
 }
